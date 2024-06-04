da386a798abe10e64ba5ea6b596a74f81f5348b3
Deepak <deekay9357@gmail.com>
2024-06-04 11:05:26 +0530

    @1.11

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+        <link rel="icon" href="/logo.ico" sizes="any" />
       </head>
       <body className={orbitron.className}>
         <ThemeProvider
